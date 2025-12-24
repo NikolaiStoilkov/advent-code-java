38530b12dda9a3dd3b97b0aeb66b0ca537b3d33f
hizzly <nikolaistoilkov98@gmail.com>
2025-12-24 15:59:05 +0200

    gift shop

--- src/main/java/org/GiftShop.java
+++ src/main/java/org/GiftShop.java
@@ -1,35 +1,79 @@
 package org;
+/*
+You get inside and take the elevator to its only other stop: the gift shop. "Thank you for visiting the North Pole!" gleefully exclaims a nearby sign. You aren't sure who is even allowed to visit the North Pole, but you know you can access the lobby through here, and from there you can access the rest of the North Pole base.
 
+As you make your way through the surprisingly extensive selection, one of the clerks recognizes you and asks for your help.
+
+As it turns out, one of the younger Elves was playing on a gift shop computer and managed to add a whole bunch of invalid product IDs to their gift shop database! Surely, it would be no trouble for you to identify the invalid product IDs for them, right?
+
+They've even checked most of the product ID ranges already; they only have a few product ID ranges (your puzzle input) that you'll need to check. For example:
+
+11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124
+
+(The ID ranges are wrapped here for legibility; in your input, they appear on a single long line.)
+
+The ranges are separated by commas (,); each range gives its first ID and last ID separated by a dash (-).
+
+Since the young Elf was just doing silly patterns, you can find the invalid IDs by looking for any ID which is made only of some sequence of digits repeated twice. So, 55 (5 twice), 6464 (64 twice), and 123123 (123 twice) would all be invalid IDs.
+
+None of the numbers have leading zeroes; 0101 isn't an ID at all. (101 is a valid ID that you would ignore.)
+
+Your job is to find all of the invalid IDs that appear in the given ranges. In the above example:
+
+    11-22 has two invalid IDs, 11 and 22.
+    95-115 has one invalid ID, 99.
+    998-1012 has one invalid ID, 1010.
+    1188511880-1188511890 has one invalid ID, 1188511885.
+    222220-222224 has one invalid ID, 222222.
+    1698522-1698528 contains no invalid IDs.
+    446443-446449 has one invalid ID, 446446.
+    38593856-38593862 has one invalid ID, 38593859.
+    The rest of the ranges contain no invalid IDs.
+
+Adding up all the invalid IDs in this example produces 1227775554.
+ */
 public class GiftShop {
     public GiftShop(){
     }
 
-    public void validateId(String input){
-        String[] splitIDs = input.split(",");
+    public long solve(String input) {
+        long invalidIdSum = 0;
 
-        for (int i = 0; i <splitIDs.length; i++) {
-            String id = splitIDs[i];
+        String[] ranges = input.trim().split(",");
 
-            if(isValidId(id)){
+        for (String range : ranges) {
+            String[] bounds = range.split("-");
+            if (bounds.length != 2) {
+                System.err.println("Skipping invalid range format: " + range);
+                continue;
+            }
 
+            long start;
+            long end;
+            try {
+                start = Long.parseLong(bounds[0]);
+                end = Long.parseLong(bounds[1]);
+            } catch (NumberFormatException e) {
+                System.err.println("Skipping invalid number in range: " + range);
+                continue;
+            }
+
+            for (long id = start; id <= end; id++) {
+                String idStr = Long.toString(id);
+                int len = idStr.length();
+                if (len % 2 == 0) {
+                    String firstHalf = idStr.substring(0, len / 2);
+                    String secondHalf = idStr.substring(len / 2);
+                    if (firstHalf.equals(secondHalf)) {
+                        invalidIdSum += id;
+                    }
+                }
             }
         }
+
+        return invalidIdSum;
     }
 
-    private Boolean isValidId(String id){
-        int firstID = Integer.parseInt(id.split("-")[0]);
-        int lastID = Integer.parseInt(id.split("-")[1]);
-
-        for (int i = firstID; i <lastID; i++) {
-
-        }
-
-        return false;
-    }
-
-    private String getPattern(String currentID){
-        switch ()
-
-        return "";
-    }
 }
